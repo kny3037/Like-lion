6a4b842af65a702f19251719137d19bbd76e9f85
psi5059 <psi505968@gmail.com>
2022-10-04 16:40:48 +0900

    for문 2씩 증가

--- src/babe_lion/ForStatement.java
+++ src/babe_lion/ForStatement.java
@@ -5,8 +5,14 @@ public class ForStatement {
     //
     public static void main(String[] args) {
       // 1 ~ 5까지 출력.
-      for(int i = 1; i <= 5; i+=1){
+        // 2, 4, 6, 8, 10
+      for(int i = 2; i <= 10; i+=2){
           System.out.println(i);
       }
+
+      int a1 = 1;
+      a1++;
+      System.out.println(a1);
+
     }
 }
