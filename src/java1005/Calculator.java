e7b4cbc75f12b7008b078f40456510335ab90069
psi5059 <psi505968@gmail.com>
2022-10-05 10:32:23 +0900

    소수점 출력 형변환

--- src/java1005/Calculator.java
+++ src/java1005/Calculator.java
@@ -12,6 +12,7 @@ public class Calculator {
         System.out.println(a * b);
     }
     public void divide(int a, int b){
-        System.out.println(a / b);
+        float result = (float) a / b;
+        System.out.println(result);
     }
 }
