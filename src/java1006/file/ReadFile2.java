0625f91f91ebf9fd504ed64dbcd6b60afa0e0381
psi5059 <psi505968@gmail.com>
2022-10-06 15:12:36 +0900

    File 목록

---
+++ src/java1006/file/ReadFile2.java
@@ -0,0 +1,20 @@
+package java1006.file;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadFile2 {
+    public static void main(String[] args) throws IOException{
+
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+    }
+}
