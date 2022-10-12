201db2bad5959d64d4294cd233711a6d972131ca
psi5059 <psi505968@gmail.com>
2022-10-12 17:29:08 +0900

    구구단 출력

---
+++ src/babe_lion/Multiply.java
@@ -0,0 +1,14 @@
+package babe_lion;
+
+public class Multiply {
+    public static void main(String[] args) {
+
+        for (int i = 1; i<10; i++){
+            System.out.println(i+"단");
+            for(int j = 1; j <= 9; j++){
+                System.out.println(i + "X" + j +"="+i*j);
+            }
+        }
+
+    }
+}
