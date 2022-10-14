0a1d0ef00d96008855d7eaf32fedda943e694086
psi5059 <psi505968@gmail.com>
2022-10-14 09:36:31 +0900

    InsertionSort 01

---
+++ src/java1014/algorithm/InsertionSort01.java
@@ -0,0 +1,24 @@
+package java1014.algorithm;
+
+import java.util.Arrays;
+
+public class InsertionSort01 {
+    public int[] sort(int[] arr){
+        int i = 1;
+        //i는 1에서 출발!
+
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        //5에서 출발!
+        int[] arr = {8, 5, 6, 2, 4};
+
+        InsertionSort01 is = new InsertionSort01();
+        int[] result = is.sort(arr);
+
+        System.out.println(Arrays.toString(result));
+
+
+
+    }
+}
