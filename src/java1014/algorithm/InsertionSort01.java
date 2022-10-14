9969537851e9cb77be36d0183a5c55c61051b5a9
psi5059 <psi505968@gmail.com>
2022-10-14 09:51:56 +0900

    InsertionSort swap 추가

--- src/java1014/algorithm/InsertionSort01.java
+++ src/java1014/algorithm/InsertionSort01.java
@@ -6,8 +6,13 @@ public class InsertionSort01 {
     public int[] sort(int[] arr){
         int i = 1;
         //i는 1에서 출발!
+        if (arr[i] < arr[i - 1]) {
+            int temp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+        }
 
-        return new int[]{};
+        return arr;
     }
     public static void main(String[] args) {
         //5에서 출발!
