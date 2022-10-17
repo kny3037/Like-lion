73510f36dcc71e675a4facca86d2ec4d62507c5f
psi5059 <psi505968@gmail.com>
2022-10-17 09:20:19 +0900

    별찍기 for문

--- src/java1017/algorithm/RightTriangle.java
+++ src/java1017/algorithm/RightTriangle.java
@@ -2,10 +2,11 @@ package java1017.algorithm;
 
 public class RightTriangle {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        for(int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
-
 }
