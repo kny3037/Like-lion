70f5d8e6c1c729dccc750f481c7ebbdfff9f6a31
psi5059 <psi505968@gmail.com>
2022-10-17 09:08:55 +0900

    별찍기 4단

---
+++ src/java1017/algorithm/RightTriangle.java
@@ -0,0 +1,11 @@
+package java1017.algorithm;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+
+}
