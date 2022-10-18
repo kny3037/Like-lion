b7dbc6e6034315bcf6ba25b75936f3162eb9efdc
psi5059 <psi505968@gmail.com>
2022-10-18 09:22:12 +0900

    정사각형, 직사각형 별찍기

--- src/java1018/algorithm/Squarstar.java
+++ src/java1018/algorithm/Squarstar.java
@@ -10,8 +10,20 @@ public class Squarstar {
         }
 
     }
+
+    void printRectangle(int x, int y){
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Squarstar squarstar = new Squarstar();
         squarstar.printSquareMatrix(4);
+        System.out.println();
+        squarstar.printRectangle(3,5);
     }
 }
